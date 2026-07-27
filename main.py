3124762916de6f776a727fe00d5a38a8fe683451
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-07-27 10:24:41 +0500

    Practice: Data Types in Python

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ print("Hello","World",sep='-', end=' ')
 print("Ali", "Gohar")
 print(5)
 print(5-3)
+
+
+# Data Types
+# Dynamic Typing
+name = "Ali"
+fname = "Gohar"
+age = 20
+print("My name is", name , fname, ", and I'm", age, "years old.")
+
+# Dynamic Binding
+age = "Twenty"
+print(age)
+
+age = 20.0
+print(age)
