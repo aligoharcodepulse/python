4795cfefa3fd77a4ae01a37986eeb4b23c549589
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-07-31 10:36:40 +0500

    Practice: Mutability and its side effects

--- intermediate.py
+++ intermediate.py
@@ -64,3 +64,56 @@ a[2]=1
 print(id(a[0]))
 print(id(a[2]))
 
+
+# Mutability
+a = "Hello" # Immutable
+print(id(a))
+a = a + "World"
+print(id(a))
+
+t = (1,2,3) # Immutable
+print(id(t))
+t = t + (5,6)
+print(id(t))
+
+l = [1,2,3] # Mutable (Memory address same after updation)
+print(id(l))
+l.append(4)
+print(id(l))
+# built-in functions work by do not changing address, whereas concat or any other oprations
+# change address
+
+# Side Effects of Mutability
+l = [1,2,3]
+l1 = l
+print(id(l))
+print(id(l1))
+l1.append(4)
+print(l)
+print(l1)
+
+# Solution is 
+l = [1,2,3]
+l1 = l[:]
+print(id(l))
+print(id(l1))
+l1.append(4)
+print(l)
+print(l1)
+
+t = (1,2,3,[4,5])
+t[-1][-1] = 500
+print(t)
+
+# l = [1,2,3,(4,5)]   does not work
+# l[-1][-1] = 500
+# print(l)
+
+l1 = [1,2]
+l2 = [3,4]
+t1  = (l1,l2)
+print(id(l1), id(l2), id(t1), sep=" ")
+t1[0][0] = 100
+print(t1)
+print(id(l1), id(l2), id(t1), sep=" ")
+
