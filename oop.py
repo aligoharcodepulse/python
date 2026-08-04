25cf4b49fe080af974b1d9de1ddea6e0c6b65f02
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-08-04 11:01:26 +0500

    Practice: Method Overloading

--- oop.py
+++ oop.py
@@ -515,3 +515,19 @@ s.review()
 
 
 
+# Method Overloading
+class Geometry:
+    def area(self, a, b=0):
+        if b==0:
+            print("Circle:", 3.14*a*a)
+        else:
+            print("Rectangle:", a*b)
+
+
+obj = Geometry()
+obj.area(4)
+obj.area(4,5)
+
+
+# Operator Overloading
+
