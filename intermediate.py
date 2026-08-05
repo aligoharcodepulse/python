20ccecfc7657bb497736d4d4bdaddeb046db75f2
Muhammad Ali Gohar <aligoharkp@gmail.com>
2026-08-05 10:00:44 +0500

    Changes in comments

--- intermediate.py
+++ intermediate.py
@@ -1,408 +1,408 @@
-# # Everything is object in python
-# # In python we call name not variable
-# a = 4 # Call by Object Reference
-# print(id(a))
-# print(id(4))
-
-# b = a # aliasing
-# print(id(b))
-
-# c = b # aliasing
-# print(id(c))
-
-# print(a)
-# print(b)
-# print(c)
-
-# del a
-# print(b)
-# print(c)
-
-# a = 5
-# b = a
-# a = 6
-# print(b)
-
-# import sys
-# u = "erherherh"
-# v = u
-# w = v
-# print(sys.getrefcount(u))
-
-
-
-# # Garbage Collector (When we delete any name/ variable, it actually delete the reference
-# # of that variable pointing towards any memory address)
-# # In order to free up the actual space in memory Garbage collector is used
-# # GC checks the address to which no variable or name points and that memory address is free
-# # It just free up that space for other processes
-
-
-# # Weird Stuff
-# # Weird Behaviour 1
-# a = 2
-# b = a
-# c = b
-# print(sys.getrefcount(a)) # just because 2 is very common and is referenced by alot of names
-
-# # WB
-# a = []
-# b = a
-# c = b
-# print(sys.getrefcount(a))
-
-# d = []
-# print(sys.getrefcount(a))
-# d = c
-# print(sys.getrefcount(a))
-
-# a = [1,2,3]
-# print(id(1))
-# print(id(a[0]))
-
-# a[2]=1
-# print(id(a[0]))
-# print(id(a[2]))
-
-
-# # Mutability
-# a = "Hello" # Immutable
-# print(id(a))
-# a = a + "World"
-# print(id(a))
-
-# t = (1,2,3) # Immutable
-# print(id(t))
-# t = t + (5,6)
-# print(id(t))
-
-# l = [1,2,3] # Mutable (Memory address same after updation)
-# print(id(l))
-# l.append(4)
-# print(id(l))
-# # built-in functions work by do not changing address, whereas concat or any other oprations
-# # change address
-
-# # Side Effects of Mutability
-# l = [1,2,3]
-# l1 = l
-# print(id(l))
-# print(id(l1))
-# l1.append(4)
-# print(l)
-# print(l1)
-
-# # Solution is 
-# l = [1,2,3]
-# l1 = l[:]
-# print(id(l))
-# print(id(l1))
-# l1.append(4)
-# print(l)
-# print(l1)
-
-# t = (1,2,3,[4,5])
-# t[-1][-1] = 500
-# print(t)
-
-# # l = [1,2,3,(4,5)]   does not work
-# # l[-1][-1] = 500
-# # print(l)
-
-# l1 = [1,2]
-# l2 = [3,4]
-# t1  = (l1,l2)
-# print(id(l1), id(l2), id(t1), sep=" ")
-# t1[0][0] = 100
-# print(t1)
-# print(id(l1), id(l2), id(t1), sep=" ")
-
-
-
-# # Functions (Abstraction and Composition)
-# def is_even(number):
-#      """
-#      This function tells if a given number is even or not
-#      input - any vali integer
-#      output - even/odd
-#      Created By - Ali
-#      Last Edited - 31st July, 2026
-#      """
-#      if type(number) == int:
-#         if(number % 2 == 0):
-#             return "Even"
-#         else:
-#             return "Odd"
-#      else:
-#         return "Not Allowed"
-
-# print(is_even("Ali"))
-# print(is_even.__doc__)
-
-
-# # Default Arguments
-# def power(a,b):
-#     return a**b
-# print(power(2,3))
-# # print(power(2)) error
-
-# # Solution is default values
-# def power(a = 1, b = 1):
-#     return a**b
-# print(power(2,3)) # Positional Arguments
-# print(power(2))
-# print(power())
-
-# # keyword arguments (has greater priority than positional arguments)
-# print(power(b=2, a=3))
-
-# # Arbitrary Arguments (eg. print function)
-# def flexi(*number): # create tuple from number
-#     product = 1
-#     print(number)
-#     for i in number:
-#         product*=i
-#     print(product)
-
-# flexi(5,4,3,2,1)
-
-# # Local and Global Variable
-# def f(y):
-#     x = 1
-#     x += 1
-#     print(x)
-
-# x = 5
-# f(x)
-# print(x)
-
-
-
-# def g(y):
-#     print(x)
-#     print(x+1)
-
-# x = 5
-# g(x)
-# print(x)
-
-
-
-# def h(y):
-#     global x # solution to update value
-#     x += 1 # Not allowed to change the value of a global variable
-
-# x = 5
-# h(x)
-# print(x)
-
-
-# # Nested Function
-# def f():
-#     print("Inside f")
-#     def g():
-#         print("Inside g")
-#     g()
-# f()
-# # g() # Error (Nested function is abstracted from main program)
-
-# # Functions as Objects
-# def eg(num):
-#     return num**2
-# c = eg # functions are objects
-# print(c(3))
-
-# del f
-# print(c(4))
-
-# l = [1,2,3,c]
-# print(l[-1](2))
-
-
-
-# def f():
-#     def x(a,b):
-#         return a+b
-#     return x
-# val = f()(3,4)
-# print(val)
-
-
-
-# # Recursion
-# def multiply(a,b):
-#     result = 0
-#     for i in range(b):
-#         result = result + a
-#     print(result)
-
-# multiply(5,4)
-
-# # Now same code through recursion
-# def multiply(a,b):
-#     if b == 1:
-#         return a
-#     else:
-#         return a + multiply(a, b-1)
-
-# print(multiply(5,6))
-
-
-# # factorial
-# def fact(number):
-#     if number == 1:
-#         return 1
-#     else:
-#         return number * fact(number-1)
-# print(fact(5))
-
-
-# # Palindrome
-# def palin(text):
-#     if len(text) <= 1:
-#         print("Palindrome")
-#     else:
-#         if(text[0] == text[-1]):
-#             palin(text[1:-1])
-#         else:
-#             print("Not a Palindrome")
-
-# palin("malayalam")
-# palin("hello")
-# palin("abba")
-
-
-# # Fabonacci
-# def fabonacci(m):
-#     if m == 0 or m == 1:
-#         return 1
-#     else:
-#         return fabonacci(m-1) + fabonacci(m-2)
-# print(fabonacci(12))
-
-
-# # Fabonacci through dynamic programming (memorization) for time complexity
-# import time
-# def memo(m, d):
-#     if m in d:
-#         return d[m]
-#     else:
-#         d[m] = memo(m-1, d) + memo(m-2, d)
-#         return d[m]
-
-# start = time.time()
-# d = {0:1, 1:1}
-# print(memo(48, d))
-# print(time.time() - start)
-
-
-
-
-# # Lambda Functions (Anonymous Functions)
-# # lambda input: expression
-# x = lambda x : x**2
-# print(x(9))
-
-# a = lambda x,y : x+y
-# print(a(9, 1))
-# print(type(a))
-
-# # Difference between normal and lambda function
-# # 1. Lambda function has no return value
-# # 2. One line
-# # 3. Not used for code reusability
-# # 4. No Name
-
-# # Why? 
-# # Along with higher order functions
-
-# b = lambda x : x[0] == 'a'
-# print(b('apple'))
-
-# c = lambda x : "Even" if x % 2 == 0 else "Odd"
-# print(c(3))
-
-# # Higher order functions
-# def return_sum(func,l):
-#     result = 0
-#     for i in l:
-#         if func(i):
-#             result+=i
-#     return result
-
-# l = [1,2,3,4,5,6,7,8,9]
-# x = lambda x : x%2 == 0
-# y = lambda x : x%2 != 0
-# z = lambda x : x%3 == 0
-
-# print(return_sum(x,l))
-# print(return_sum(y,l))
-# print(return_sum(z,l))
-
-
-
-# # Map function
-# l = [1,2,3,4,5]
-# print(list(map(lambda x : x*2, l)))
-
-# print(list(map(lambda x : x%2 == 0, l)))
-
-# students = [
-#     {
-#         "Name": "Ali",
-#         "Father Name": "Gohar Ali"
-#     },
-#     {
-#         "Name": "Ahmad",
-#         "Father Name": "Yasin"
-#     }
-# ]
-
-# print(list(map(lambda student : student["Name"], students)))
-
-
-
-# # Filter Function
-# l = [1,2,3,4,5,6,7]
-# print(list(filter(lambda x:x>5, l)))
-
-# fruits = ['Apple', 'Orange', 'Mango', 'Guava']
-# print(list(filter(lambda fruit: 'e' in fruit, fruits)))
-
-
-# # Reduce Function
-# import functools
-# print(functools.reduce(lambda x,y : x+y, l))
-
-# l1 = [12,34,56,11,21,58]
-# print(functools.reduce(lambda x,y: x if x>y else y, l1 ))
-
-# print(functools.reduce(lambda x,y: x if x<y else y, l1 ))
-
-# # List Comprehension
-# l = [1,2,3,4,5]
-# l = [item * 2 for item in l]
+# Everything is object in python
+# In python we call name not variable
+a = 4 # Call by Object Reference
+print(id(a))
+print(id(4))
+
+b = a # aliasing
+print(id(b))
+
+c = b # aliasing
+print(id(c))
+
+print(a)
+print(b)
+print(c)
+
+del a
+print(b)
+print(c)
+
+a = 5
+b = a
+a = 6
+print(b)
+
+import sys
+u = "erherherh"
+v = u
+w = v
+print(sys.getrefcount(u))
+
+
+
+# Garbage Collector (When we delete any name/ variable, it actually delete the reference
+# of that variable pointing towards any memory address)
+# In order to free up the actual space in memory Garbage collector is used
+# GC checks the address to which no variable or name points and that memory address is free
+# It just free up that space for other processes
+
+
+# Weird Stuff
+# Weird Behaviour 1
+a = 2
+b = a
+c = b
+print(sys.getrefcount(a)) # just because 2 is very common and is referenced by alot of names
+
+# WB
+a = []
+b = a
+c = b
+print(sys.getrefcount(a))
+
+d = []
+print(sys.getrefcount(a))
+d = c
+print(sys.getrefcount(a))
+
+a = [1,2,3]
+print(id(1))
+print(id(a[0]))
+
+a[2]=1
+print(id(a[0]))
+print(id(a[2]))
+
+
+# Mutability
+a = "Hello" # Immutable
+print(id(a))
+a = a + "World"
+print(id(a))
+
+t = (1,2,3) # Immutable
+print(id(t))
+t = t + (5,6)
+print(id(t))
+
+l = [1,2,3] # Mutable (Memory address same after updation)
+print(id(l))
+l.append(4)
+print(id(l))
+# built-in functions work by do not changing address, whereas concat or any other oprations
+# change address
+
+# Side Effects of Mutability
+l = [1,2,3]
+l1 = l
+print(id(l))
+print(id(l1))
+l1.append(4)
+print(l)
+print(l1)
+
+# Solution is 
+l = [1,2,3]
+l1 = l[:]
+print(id(l))
+print(id(l1))
+l1.append(4)
+print(l)
+print(l1)
+
+t = (1,2,3,[4,5])
+t[-1][-1] = 500
+print(t)
+
+# l = [1,2,3,(4,5)]   does not work
+# l[-1][-1] = 500
 # print(l)
 
-# l2 = [i**2 for i in range(10)]
-# print(l2)
+l1 = [1,2]
+l2 = [3,4]
+t1  = (l1,l2)
+print(id(l1), id(l2), id(t1), sep=" ")
+t1[0][0] = 100
+print(t1)
+print(id(l1), id(l2), id(t1), sep=" ")
 
-# l3 = [i**2 for i in range(10) if i%2==0]
-# print(l3)
 
-# l4 = [fruit for fruit in fruits if fruit[0]=='O']
-# print(l4)
 
-# # Dictionary Comprehension
-# d = {"Name":"Ali", "Gender":"Male", "Age":20}
-# print(d.items())
+# Functions (Abstraction and Composition)
+def is_even(number):
+     """
+     This function tells if a given number is even or not
+     input - any vali integer
+     output - even/odd
+     Created By - Ali
+     Last Edited - 31st July, 2026
+     """
+     if type(number) == int:
+        if(number % 2 == 0):
+            return "Even"
+        else:
+            return "Odd"
+     else:
+        return "Not Allowed"
 
-# d1 = {key:value for key,value in d.items() if len(key)>3}
-# print(d1)
+print(is_even("Ali"))
+print(is_even.__doc__)
 
-# print(l)
-# d2 = {item:item/2 for item in l}
-# print(d2)
+
+# Default Arguments
+def power(a,b):
+    return a**b
+print(power(2,3))
+# print(power(2)) error
+
+# Solution is default values
+def power(a = 1, b = 1):
+    return a**b
+print(power(2,3)) # Positional Arguments
+print(power(2))
+print(power())
+
+# keyword arguments (has greater priority than positional arguments)
+print(power(b=2, a=3))
+
+# Arbitrary Arguments (eg. print function)
+def flexi(*number): # create tuple from number
+    product = 1
+    print(number)
+    for i in number:
+        product*=i
+    print(product)
+
+flexi(5,4,3,2,1)
+
+# Local and Global Variable
+def f(y):
+    x = 1
+    x += 1
+    print(x)
+
+x = 5
+f(x)
+print(x)
+
+
+
+def g(y):
+    print(x)
+    print(x+1)
+
+x = 5
+g(x)
+print(x)
+
+
+
+def h(y):
+    global x # solution to update value
+    x += 1 # Not allowed to change the value of a global variable
+
+x = 5
+h(x)
+print(x)
+
+
+# Nested Function
+def f():
+    print("Inside f")
+    def g():
+        print("Inside g")
+    g()
+f()
+# g() # Error (Nested function is abstracted from main program)
+
+# Functions as Objects
+def eg(num):
+    return num**2
+c = eg # functions are objects
+print(c(3))
+
+del f
+print(c(4))
+
+l = [1,2,3,c]
+print(l[-1](2))
+
+
+
+def f():
+    def x(a,b):
+        return a+b
+    return x
+val = f()(3,4)
+print(val)
+
+
+
+# Recursion
+def multiply(a,b):
+    result = 0
+    for i in range(b):
+        result = result + a
+    print(result)
+
+multiply(5,4)
+
+# Now same code through recursion
+def multiply(a,b):
+    if b == 1:
+        return a
+    else:
+        return a + multiply(a, b-1)
+
+print(multiply(5,6))
+
+
+# factorial
+def fact(number):
+    if number == 1:
+        return 1
+    else:
+        return number * fact(number-1)
+print(fact(5))
+
+
+# Palindrome
+def palin(text):
+    if len(text) <= 1:
+        print("Palindrome")
+    else:
+        if(text[0] == text[-1]):
+            palin(text[1:-1])
+        else:
+            print("Not a Palindrome")
+
+palin("malayalam")
+palin("hello")
+palin("abba")
+
+
+# Fabonacci
+def fabonacci(m):
+    if m == 0 or m == 1:
+        return 1
+    else:
+        return fabonacci(m-1) + fabonacci(m-2)
+print(fabonacci(12))
+
+
+# Fabonacci through dynamic programming (memorization) for time complexity
+import time
+def memo(m, d):
+    if m in d:
+        return d[m]
+    else:
+        d[m] = memo(m-1, d) + memo(m-2, d)
+        return d[m]
+
+start = time.time()
+d = {0:1, 1:1}
+print(memo(48, d))
+print(time.time() - start)
+
+
+
+
+# Lambda Functions (Anonymous Functions)
+# lambda input: expression
+x = lambda x : x**2
+print(x(9))
+
+a = lambda x,y : x+y
+print(a(9, 1))
+print(type(a))
+
+# Difference between normal and lambda function
+# 1. Lambda function has no return value
+# 2. One line
+# 3. Not used for code reusability
+# 4. No Name
+
+# Why? 
+# Along with higher order functions
+
+b = lambda x : x[0] == 'a'
+print(b('apple'))
+
+c = lambda x : "Even" if x % 2 == 0 else "Odd"
+print(c(3))
+
+# Higher order functions
+def return_sum(func,l):
+    result = 0
+    for i in l:
+        if func(i):
+            result+=i
+    return result
+
+l = [1,2,3,4,5,6,7,8,9]
+x = lambda x : x%2 == 0
+y = lambda x : x%2 != 0
+z = lambda x : x%3 == 0
+
+print(return_sum(x,l))
+print(return_sum(y,l))
+print(return_sum(z,l))
+
+
+
+# Map function
+l = [1,2,3,4,5]
+print(list(map(lambda x : x*2, l)))
+
+print(list(map(lambda x : x%2 == 0, l)))
+
+students = [
+    {
+        "Name": "Ali",
+        "Father Name": "Gohar Ali"
+    },
+    {
+        "Name": "Ahmad",
+        "Father Name": "Yasin"
+    }
+]
+
+print(list(map(lambda student : student["Name"], students)))
+
+
+
+# Filter Function
+l = [1,2,3,4,5,6,7]
+print(list(filter(lambda x:x>5, l)))
+
+fruits = ['Apple', 'Orange', 'Mango', 'Guava']
+print(list(filter(lambda fruit: 'e' in fruit, fruits)))
+
+
+# Reduce Function
+import functools
+print(functools.reduce(lambda x,y : x+y, l))
+
+l1 = [12,34,56,11,21,58]
+print(functools.reduce(lambda x,y: x if x>y else y, l1 ))
+
+print(functools.reduce(lambda x,y: x if x<y else y, l1 ))
+
+# List Comprehension
+l = [1,2,3,4,5]
+l = [item * 2 for item in l]
+print(l)
+
+l2 = [i**2 for i in range(10)]
+print(l2)
+
+l3 = [i**2 for i in range(10) if i%2==0]
+print(l3)
+
+l4 = [fruit for fruit in fruits if fruit[0]=='O']
+print(l4)
+
+# Dictionary Comprehension
+d = {"Name":"Ali", "Gender":"Male", "Age":20}
+print(d.items())
+
+d1 = {key:value for key,value in d.items() if len(key)>3}
+print(d1)
+
+print(l)
+d2 = {item:item/2 for item in l}
+print(d2)
 
 
 # Threading
@@ -462,3 +462,5 @@ withdraw_thread.start()
 
 deposit_thread.join()
 withdraw_thread.join()
+
+
